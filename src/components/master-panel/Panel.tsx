905ce6f9016dc112c69a7a1776967696f5739f3c
Shi-Jia-Ming <sjiaming38@gmail.com>
2024-10-27 23:53:04 +0800

    feat: add master panel component

---
+++ src/components/master-panel/Panel.tsx
@@ -0,0 +1,43 @@
+import { ReactElement } from "react";
+
+export const LeftPanel = ({
+  children,
+  className,
+}: {
+  children?: ReactElement;
+  className?: string;
+}) => {
+  return (
+    <div id={"left-panel"} className={`${className} size-full select-none`}>
+      {children}
+    </div>
+  );
+};
+
+export const RightPanel = ({
+  children,
+  className,
+}: {
+  children?: ReactElement;
+  className?: string;
+}) => {
+  return (
+    <div id={"right-panel"} className={`${className} size-full select-none`}>
+      {children}
+    </div>
+  );
+};
+
+export const MainPanel = ({
+  children,
+  className,
+}: {
+  children?: ReactElement;
+  className?: string;
+}) => {
+  return (
+    <div id={"main-panel"} className={`${className} size-full select-none`}>
+      {children}
+    </div>
+  );
+};
